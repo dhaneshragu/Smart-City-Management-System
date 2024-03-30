ec89d0b3076860f0fa9705a252db49e962d086f3
unknown <vijeshrbhat@gmail.com>
2024-03-30 16:54:17 +0530

    employment portal user backend

--- SmartCityMgmtSystem/SmartCityMgmtSystem/My Project/Application.Designer.vb
+++ SmartCityMgmtSystem/SmartCityMgmtSystem/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.SmartCityMgmtSystem.Employment_portal
+            Me.MainForm = Global.SmartCityMgmtSystem.UserLandingPage
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
