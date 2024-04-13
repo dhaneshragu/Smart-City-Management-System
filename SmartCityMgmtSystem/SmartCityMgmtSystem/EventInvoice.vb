7d29d61d2171b4b9b37aabee74a2789f3d6a4f0f
KewangZhili <hdekamama11@gmail.com>
2024-04-13 14:32:16 +0900

    Modified Events Module

--- SmartCityMgmtSystem/SmartCityMgmtSystem/EventInvoice.vb
+++ SmartCityMgmtSystem/SmartCityMgmtSystem/EventInvoice.vb
@@ -1,28 +1,37 @@
 ﻿Public Class EventInvoice
-    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+    Public Property uid As Integer
+    Private Property customerID As Integer
+    Private Property vendorID As Integer
+    Private Property transactionID As String
+    Private Property cost As Integer
+    Private Property duration As Integer
+    Private Property vendorName As String
+    Private Property customerName As String
 
-
+    Public Sub New(ByVal customerID As Integer, ByVal vendorID As Integer, ByVal transactionID As String, ByVal cost As Integer, ByVal duration As Integer, ByVal vendorName As String, ByVal customerName As String)
+        InitializeComponent()
+        Me.customerID = customerID
+        Me.vendorID = vendorID
+        Me.transactionID = transactionID
+        Me.cost = cost
+        Me.duration = duration
+        Me.vendorName = vendorName
+        Me.customerName = customerName
     End Sub
 
-    Private Sub Label11_Click(sender As Object, e As EventArgs) Handles eventID.Click
-
-    End Sub
-
-    Private Sub bank_Click(sender As Object, e As EventArgs) Handles bank.Click
-
-    End Sub
-
-    Private Sub tax_Click(sender As Object, e As EventArgs) Handles tax.Click
-
-    End Sub
 
     Private Sub Invoice_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim row As New DataGridViewRow()
         DataGridView1.Rows.Add(row)
 
-        DataGridView1.Rows(0).Cells("TRANSACTIONID").Value = 902749224
-        DataGridView1.Rows(0).Cells("RATE").Value = 1024
-        DataGridView1.Rows(0).Cells("DAYS").Value = 4
-        DataGridView1.Rows(0).Cells("AMOUNT").Value = 4096
+        DataGridView1.Rows(0).Cells("TRANSACTION_ID").Value = transactionID
+        DataGridView1.Rows(0).Cells("RATE").Value = cost
+        DataGridView1.Rows(0).Cells("DAYS").Value = duration
+        DataGridView1.Rows(0).Cells("AMOUNT").Value = cost * duration
+        total.Text = cost * duration
+        customerDescription.Text = customerName
+        vendorAddress.Text = vendorName
     End Sub
+
+
 End Class
